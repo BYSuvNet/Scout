2c262f8d66db5cf6a2e3acf858927ea066d02e19
Nasstrom33 <nasstrom.alex@gmail.com>
2024-10-24 10:05:01 +0200

    nothing

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using ScoutApp.UI;
 
 ScoutRepository repo = new(); //Starta grundfunktionalitet
+
 ScoutUI ui = new(repo); //Start UI och ge den referens till grundsystemet
 
 ui.Run(); //Kör igång huvudloopen
