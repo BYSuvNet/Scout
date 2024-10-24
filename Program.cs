aaa5816b4d59812c1c2d4ced98c2a2c6f9ae1670
Brosaidon <jos.lidb3ck@gmail.com>
2024-10-24 10:00:31 +0200

    🎨 add :D

--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@ ScoutRepository repo = new(); //Starta grundfunktionalitet
 ScoutUI ui = new(repo); //Start UI och ge den referens till grundsystemet
 
 ui.Run(); //Kör igång huvudloopen
+Console.WriteLine(":D");
