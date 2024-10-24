0ad258482df7bc58e76e2814fde532cbf4757b39
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2024-10-24 10:43:24 +0200

    förändring

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using ScoutApp.UI;
 
 ScoutRepository repo = new(); //Starta grundfunktionalitet
 ScoutUI ui = new(repo); //Start UI och ge den referens till grundsystemet
-Scout nyscout = new() { Name = "Alex", Email = "Gmal.com" };
+Scout nyscout = new() { Name = "Rolf", Email = "Gmal.com" };
 string json = JsonSerializer.Serialize(nyscout);
 File.WriteAllText("scouter.json", json);
 ui.Run(); //Kör igång huvudloopen
